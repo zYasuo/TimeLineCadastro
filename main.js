ee39ce913c3c8f18b4537f4cb861f22d9ea2cd25
Danilo S. Aguiar (1762) <daguiar@nrtelecom.com.br>
2023-06-01 15:29:21 -0300

    Codigo JS refatorado e adicionado novas funções

--- main.js
+++ main.js
@@ -1,80 +1,67 @@
-const barFilled = document.querySelector('.timeline .bar-filled');
-const steps = document.querySelectorAll('.step');
-const formParts = document.querySelectorAll('.form-part');
-const nextButtons = document.querySelectorAll('.next');
-const bar = document.querySelector('.bar');
+const steps = Array.from(document.querySelectorAll('.step'));
+const formParts = Array.from(document.querySelectorAll('.form-part'));
+const nextButtons = Array.from(document.querySelectorAll('.next'));
+const progressBar = document.querySelector('.bar-filled');
+const requiredFields = Array.from(document.querySelectorAll('input[required]'));
+const progressPerStep = 100 / (steps.length - 1);
+
 let currentStep = 1;
 
-// Adiciona a classe "active" à barra de progresso
-bar.classList.add('active');
+requiredFields.forEach(field => field.addEventListener('input', updateProgressBar));
 
-// Função para avançar para a próxima parte do formulário
-function goToNextFormPart() {
-   const currentFormPart = formParts[currentStep - 1];
-   const requiredInputs = currentFormPart.querySelectorAll('input[required]');
-   let allRequiredFieldsFilled = true;
-
-   requiredInputs.forEach(input => {
-       if (input.value.trim() === '') {
-           allRequiredFieldsFilled = false;
-       }
-   });
-
-   if (!allRequiredFieldsFilled) {
-       alert('Por favor, preencha todos os campos obrigatórios antes de prosseguir.');
-       return;
-   }
-
-   if (currentStep >= steps.length) return;
-
-   // Oculta a parte do formulário atual
-   formParts[currentStep - 1].classList.remove('active');
-
-   // Avança para a próxima parte do formulário
-   steps[currentStep].classList.add('active');
-   formParts[currentStep].classList.add('active');
-   currentStep++;
-
-   // Atualiza o estilo da barra de progresso
-   updateProgressBar();
+function allFieldsFilled(formPart) {
+  const allFilled = Array.from(formPart.querySelectorAll('input[required]')).every(input => input.value.trim() !== '');
+  formPart.classList.toggle('completed', allFilled);
+  const index = formParts.indexOf(formPart);
+  steps[index].classList.toggle('completed-step', allFilled);
+  return allFilled;
 }
 
-// Adiciona um listener para o clique do botão "Próximo"
-nextButtons.forEach(button => {
-   button.addEventListener('click', () => {
-       goToNextFormPart();
-   });
-});
+function changeStep(newStep) {
+  if (newStep > currentStep && !allFieldsFilled(formParts[currentStep - 1])) {
+    alert('Por favor, preencha todos os campos obrigatórios antes de prosseguir.');
+    return;
+  }
 
-// Adiciona um listener para o clique nas etapas da timeline
-steps.forEach(step => {
-   step.addEventListener('click', () => {
-       const stepNumber = Number(step.getAttribute('data-step'));
-       if (stepNumber > currentStep) return;
-       currentStep = stepNumber;
+  formParts[currentStep - 1].classList.remove('active');
+  steps[currentStep - 1].classList.remove('active');
 
-       // Remove a classe "active" de todas as partes do formulário e etapas da timeline
-       formParts.forEach(part => {
-           part.classList.remove('active');
-       });
-       steps.forEach(s => {
-           s.classList.remove('active');
-       });
+  currentStep = newStep;
 
-       // Adiciona a classe "active" à etapa da timeline e parte do formulário corretas
-       step.classList.add('active');
-       formParts[currentStep - 1].classList.add('active');
+  formParts[currentStep - 1].classList.add('active');
+  steps[currentStep - 1].classList.add('active');
+  updateProgressBar();
 
-       // Atualiza o estilo da barra de progresso
-       updateProgressBar();
-   });
-});
+  steps.forEach((step, index) => {
+    const isCurrentStep = index === currentStep - 1;
+    const isPreviousStep = index < currentStep;
+    step.classList.toggle('completed-step', isPreviousStep);
+    step.classList.toggle('highlight', isPreviousStep);
+    step.classList.toggle('active', isCurrentStep);
+  });
+}
 
 function updateProgressBar() {
-   const progress = ((currentStep - 1) / (steps.length - 1)) * 100;
-   barFilled.style.width = `${progress}%`;
+  let totalProgress = 0;
+
+  for (let i = 0; i < currentStep; i++) {
+    const fieldsInStep = formParts[i].querySelectorAll('input[required]');
+    const filledFieldsInStep = Array.from(fieldsInStep).filter(field => field.value.trim() !== '').length;
+    totalProgress += (filledFieldsInStep / fieldsInStep.length) * progressPerStep;
+  }
+
+  progressBar.style.width = `${totalProgress}%`;
 }
 
-steps[0].classList.add('active');
-formParts[0].classList.add('active');
-barFilled.classList.add('active');
+steps.forEach((step, index) => step.addEventListener('click', () => changeStep(index + 1)));
+nextButtons.forEach((button, index) => button.addEventListener('click', (e) => {
+  e.preventDefault();
+  changeStep(index + 2);
+}));
+
+document.addEventListener('DOMContentLoaded', function () {
+  const elems = document.querySelectorAll('select');
+  const instances = M.FormSelect.init(elems);
+});
+
+changeStep(currentStep);
